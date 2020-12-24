4acd0105ee208c2ac0f4faeb3534c9d3d4fda3bd
company <476567162@qq.com>
2020-12-24 17:34:57 +0800

    项目整体框架搭建整体设计一1.0

---
+++ msb-dongbao-generator/src/main/java/com/mashibing/msbdongbaogenerator/MsbDongbaoGeneratorApplication.java
@@ -0,0 +1,11 @@
+package com.mashibing.msbdongbaogenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbDongbaoGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsbDongbaoGeneratorApplication.class, args);
+    }
+}
