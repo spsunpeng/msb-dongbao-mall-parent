44b53d5d4bcf979f2a20eae325aecb3dbcec5b97
company <476567162@qq.com>
2020-12-28 17:18:43 +0800

    测试版本回退

---
+++ msb-dongbao-service/msb-dongbao-ums/src/test/java/com/msb/msbdongbaoums/test/Test1.java
@@ -0,0 +1,11 @@
+package com.msb.msbdongbaoums.test;
+
+/**
+ * @author sunpeng
+ * @Date 2020-12-28 17:16
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello sunpeng");
+    }
+}
