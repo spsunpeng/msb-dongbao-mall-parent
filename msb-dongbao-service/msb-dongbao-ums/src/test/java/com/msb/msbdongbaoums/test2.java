5982376609502bc80d62dae300d6283ca3f28cd6
company <476567162@qq.com>
2020-12-29 17:30:03 +0800

    develop提交2

--- msb-dongbao-service/msb-dongbao-ums/src/test/java/com/msb/msbdongbaoums/test2.java
+++ msb-dongbao-service/msb-dongbao-ums/src/test/java/com/msb/msbdongbaoums/test2.java
@@ -4,7 +4,7 @@ package com.msb.msbdongbaoums;
  * @author sunpeng
  * @Date 2020-12-29 17:11
  */
-public class Test2 {
+public class test2 {
     public static void main(String[] args) {
         System.out.println("hello sunpeng");
     }
