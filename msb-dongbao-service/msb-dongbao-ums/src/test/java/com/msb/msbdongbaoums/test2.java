e28a571e61d88ee9b75cc09050604f65795362ad
company <476567162@qq.com>
2020-12-29 17:32:38 +0800

    feature-1提交1

---
+++ msb-dongbao-service/msb-dongbao-ums/src/test/java/com/msb/msbdongbaoums/test2.java
@@ -0,0 +1,11 @@
+package com.msb.msbdongbaoums;
+
+/**
+ * @author sunpeng
+ * @Date 2020-12-29 17:31
+ */
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
