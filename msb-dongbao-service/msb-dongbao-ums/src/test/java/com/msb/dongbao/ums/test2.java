c01830f22c06197f5abf956f87bbaa8408fdb172
sunpengHome <476567162@qq.com>
2021-01-27 20:03:06 +0800

    test2

--- msb-dongbao-service/msb-dongbao-ums/src/test/java/com/msb/dongbao/ums/test2.java
+++ msb-dongbao-service/msb-dongbao-ums/src/test/java/com/msb/dongbao/ums/test2.java
@@ -8,5 +8,6 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("develop");
         System.out.println("test");
+        System.out.println("test2");
     }
 }
